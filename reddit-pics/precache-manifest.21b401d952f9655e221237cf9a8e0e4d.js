e6f5cbf9f53590db36b2dbb58f50cac8514038ec
Vladimir Kadalashvili <Kadalashvili.Vladimir@gmail.com>
2020-06-17 22:10:46 +0500

    Reddit pics demo

---
+++ reddit-pics/precache-manifest.21b401d952f9655e221237cf9a8e0e4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc1ee83497fec07f80bf4dbf9c57d191",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffceb0764d49ab32a0cb",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "ffceb0764d49ab32a0cb",
+    "url": "/static/js/2.fd8fb102.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fd8fb102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8de041ae5f233caa44af",
+    "url": "/static/js/main.1fa64201.chunk.js"
+  },
+  {
+    "revision": "adf3cdaee15964d1de76",
+    "url": "/static/js/runtime-main.f6e12260.js"
+  }
+]);
